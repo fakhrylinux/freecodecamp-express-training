3372fee313b382dc7a2402de1486ea92a03631fa
fakhry <fakhry.linux@gmail.com>
2022-03-02 23:07:54 +0900

    add .env

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 var express = require('express')
 var app = express()
 
@@ -14,9 +15,15 @@ app.get('/', function (req, res) {
 app.use('/public', express.static(__dirname + '/public'))
 
 app.get('/json', function (req, res) {
-  res.json({
-    'message': 'Hello json',
-  })
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
+    res.json({
+      'message': 'HELLO JSON',
+    })
+  } else {
+    res.json({
+      'message': 'Hello json',
+    })
+  }
 })
 
 module.exports = app
